73b95063427acb6f935fbcdcbfb60cd62a915640
YellowFear333 <yagojpa03@hotmail.com>
2019-06-28 13:52:02 -0300

    first commit

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPage } from './login.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {}
+
+// https://forum.ionicframework.com/t/ionic-4-star-rating-styling/162348
